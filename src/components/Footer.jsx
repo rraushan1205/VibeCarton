623bddde178853fc9f7ea811b230090ed93cfd70
unknown <raushanraj1205@gmail.com>
2024-09-19 22:52:18 +0530

    added navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="bg-blue-300 w-[100%] h-52 px-10 mt-5 text-justify py-5">
       <article className="flex justify-center mt-5">
         <ul className="flex text-white ">
-          <li className="hover:translate-y-[-10px] transition-all duration-[200ms]">
+          <li className="hover:translate-y-[-10px] transition-all duration-[200ms] hover:shadow-[0_18px_18px_-7px_rgba(0,0,0,0.3)]">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="white"
@@ -26,7 +26,7 @@ function Footer() {
             </svg>
           </li>
           
-          <li className="hover:translate-y-[-10px] transition-all duration-[200ms]">
+          <li className="hover:translate-y-[-10px] transition-all duration-[200ms] hover:shadow-[0_18px_18px_-7px_rgba(0,0,0,0.3)]">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="40"
@@ -41,7 +41,7 @@ function Footer() {
               <ellipse cx="6.5" cy="6.5" rx="1.55" ry="1.5"></ellipse>
             </svg>
           </li>
-          <li className="hover:translate-y-[-10px] transition-all duration-[200ms]">
+          <li className="hover:translate-y-[-10px] transition-all duration-[200ms] hover:shadow-[0_18px_18px_-7px_rgba(0,0,0,0.3)]">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="white"
@@ -55,7 +55,7 @@ function Footer() {
               <path d="M18 3H6C4.3 3 3 4.3 3 6v12c0 1.7 1.3 3 3 3h12c1.7 0 3-1.3 3-3V6c0-1.7-1.3-3-3-3zm-6 6c1.7 0 3 1.3 3 3s-1.3 3-3 3-3-1.3-3-3 1.3-3 3-3zm3.8-2c0-.7.6-1.2 1.2-1.2s1.2.6 1.2 1.2-.5 1.2-1.2 1.2-1.2-.5-1.2-1.2zM18 19H6c-.6 0-1-.4-1-1v-6h2c0 2.8 2.2 5 5 5s5-2.2 5-5h2v6c0 .6-.4 1-1 1z"></path>
             </svg>
           </li>
-          <li className="hover:translate-y-[-10px] transition-all duration-[200ms] hover:shadow-[0_3px_10px_1px_rgba(0,0,0,0.3)]">
+          <li className="hover:translate-y-[-10px] transition-all duration-[200ms] hover:shadow-[0_18px_18px_-7px_rgba(0,0,0,0.3)]">
           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" id="twitter" viewBox="0 0 100 100">
   <path fill="#fff" d="M98.488 9.473a42.1 42.1 0 0 1-11.634 3.189A20.078 20.078 0 0 0 95.736 1.5a40.35 40.35 0 0 1-12.8 4.887A20.189 20.189 0 0 0 48.007 20.2a20.79 20.79 0 0 0 .468 4.6A57.15 57.15 0 0 1 6.857 3.681a20.2 20.2 0 0 0 6.2 26.986 19.94 19.94 0 0 1-9.118-2.487v.22a20.284 20.284 0 0 0 16.177 19.841 20.152 20.152 0 0 1-5.294.665A17.853 17.853 0 0 1 11 48.561a20.383 20.383 0 0 0 18.867 14.065 40.57 40.57 0 0 1-25.034 8.612A37.819 37.819 0 0 1 0 70.96a56.843 56.843 0 0 0 30.974 9.061c37.154 0 57.468-30.777 57.468-57.455 0-.893-.031-1.754-.074-2.61a40.28 40.28 0 0 0 10.12-10.483Z"></path>
 </svg>
